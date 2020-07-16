f6c0efa58ae45e97edd4bebbb58c4520e0934540
Alpz <61177044+Alpz-dev@users.noreply.github.com>
2020-07-16 19:35:06 -0400

    Updated comments
    Added comments to each function within the Data  object

--- data.py
+++ data.py
@@ -10,6 +10,7 @@ class Data(object):
         self.y = np.array(y)
         self.peaks = []
 
+    # Very basic plotting to test data changes
     def plot(self, figure, title='', start_x=None, end_x=None):
         try:
             tmp_y_data = self.y[int(start_x - self.x[0]):int((end_x + 1) - self.x[0])]
@@ -23,6 +24,7 @@ class Data(object):
         plt.title(str(title))
         return plt.plot(tmp_x_data, tmp_y_data)
 
+    # Finds the average value of y across an x range
     def mean(self, start_x=None, end_x=None):
         try:
             tmp_data = self.y[int(start_x - self.x[0]):int((end_x + 1) - self.x[0])]
@@ -30,6 +32,7 @@ class Data(object):
             tmp_data = self.y
         return np.mean(tmp_data)
 
+    # Finds standard deviation of y across an x range
     def std(self, start_x=None, end_x=None):
         try:
             tmp_data = self.y[int(start_x - self.x[0]):int((end_x + 1) - self.x[0])]
@@ -37,12 +40,17 @@ class Data(object):
             tmp_data = self.y
         return np.std(tmp_data)
 
+    # Finds the signal to noise ratio of y across an x range
+    # Compares the mean of the data (taken as the ground truth) to the standard deviation
+    # Note: ideal for flat or constant sections of data (possible to use 1st or 2nd derivative if no constant section
+    # in base data
     def snr(self, start_x=None, end_x=None):
         mean = abs(self.mean(start_x=start_x, end_x=end_x))
         std = self.std(start_x=start_x, end_x=end_x)
         SNR = mean / std
         return SNR / (1 + SNR)
 
+    # Applies the savgol filter from the SciPy package across an x range
     def savgol_filter(self, window, order, deriv=0, start_x=None, end_x=None):
         deriv_pass = deriv
         try:
@@ -51,6 +59,7 @@ class Data(object):
             tmp_data = self.y
         return Data(self.x, np.array(signal.savgol_filter(tmp_data, window, order, deriv=deriv_pass)))
 
+    # Finds the error between two data sets across an x range
     def err(self, edited_data, start_x=None, end_x=None):
         try:
             start_i = int(start_x - self.x[0])
@@ -64,6 +73,8 @@ class Data(object):
             err += r
         return err
 
+    # Finds the nearest odd of an integer
+    # Defaults to nearest odd <= integer
     def nearest_odd(self, int, round_down=True):
         if int % 2 == 1:
             return int
@@ -73,12 +84,17 @@ class Data(object):
             else:
                 return int + 1
 
+    # Adds simulated noise to a data set using a random gaussian distribution
     def sim_noise(self, level):
         tmp_vals = []
         for i in range(len(self.y)):
             tmp_vals.append(random.gauss(self.y[i], self.y[i] * (level / 100)))
         return Data(self.x, tmp_vals)
 
+    # Naive search for the optimized values for the savgol filter function
+    # Finds all possible values and finds the smallest measure
+    # Measure = Error / (SNR^2) <- minimize error and maximize signal to noise ratio
+    # Note: Can be slow for very large ranges
     def optimized_smooth(self, err_range, snr_range):
         measures = []
         index = []
@@ -95,6 +111,7 @@ class Data(object):
         vals = index[measures.index(min(measures))]
         return self.savgol_filter(vals[0], vals[1])
 
+    # Recursively finds the maximum y value within a given delta of an x value
     def isMaxinDelta(self, i, delta=200):
         first = 0
         last = len(self.x) - 1
@@ -111,15 +128,20 @@ class Data(object):
         self.peaks.append(self.x[i])
         return True
 
+    # WIP
+    # Locates peaks using a smooth 1st derivative
     def findLocalMaxima(self):
         deriv1 = self.savgol_filter(35, 2, deriv=1)
         for i in range(len(deriv1.y) - 1):
             y1 = deriv1.y[i]
             y2 = deriv1.y[i + 1]
             if (y1 > 0 and y2 < 0) and (self.isMaxinDelta(i)):
-                #self.peaks.append(self.data['X'][i])
+                # self.peaks.append(self.data['X'][i])
                 pass
 
+
+# Imports *.txt UV-Vis data from the Agilent ChemStation v10.0.1 software for Windows XP
+# Creates a data obj containing x and y values from the file
 def import_data(fileName):
     data = open(fileName, "r")
     x = []
@@ -133,16 +155,8 @@ def import_data(fileName):
         if hasAlpha == False:
             for i in range(len(line)):
                 if line[i].isspace():
-                    try:
-                        x_val = float(line[:i])
-                    except ValueError:
-                        pass
-                    try:
-                        y_val = float(line[i + 1:])
-                    except ValueError:
-                        pass
+                    x_val = float(line[:i])
+                    y_val = float(line[i + 1:])
                     x.append(x_val)
                     y.append(y_val)
     return Data(x, y)
-
-
